385278961c325f4504ad174afeb464cc191729a2
nathantkn <trkhoinguyen.ptcnn@gmail.com>
2025-09-24 16:44:50 -0500

    initial skeleton structure setup

---
+++ src/data/teamMembers.ts
@@ -0,0 +1,140 @@
+export const teamMembers = [
+  {
+    name: "Naga Maddipudi",
+    title: "Director",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Kaito Sekiya",
+    title: "Director",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Kaustubha Medikundam",
+    title: "Communications - Team Lead",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Florianne Che",
+    title: "Web Development - Team Lead",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Nathan Trinh",
+    title: "Web Development - Team Lead",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Eman Arsham",
+    title: "Outreach",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Niharika Patil",
+    title: "Outreach",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Huda Syed",
+    title: "Outreach",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Mina Pattanaik",
+    title: "Outreach",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Gerard Wilbert",
+    title: "Logistics",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Mehtab Kaur",
+    title: "Logistics",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Stanley Huang",
+    title: "Logistics",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Sena Ozcan",
+    title: "Logistics",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Kavya Patel",
+    title: "Communications",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Zaina Khalil",
+    title: "Communications",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Rajvi Shah",
+    title: "Experience",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Rasleen Dhaliwal",
+    title: "Experience",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Adithya Prasad",
+    title: "Experience",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Rawisara Hameyotin",
+    title: "Experience",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Dominic Irla",
+    title: "Web Development",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Josephine Lee",
+    title: "Web Development",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Daniel Barajas",
+    title: "Web Development",
+    image: "",
+    linkedin: ""
+  },
+  {
+    name: "Fatima Rasheed",
+    title: "Web Development",
+    image: "",
+    linkedin: ""
+  },
+]
