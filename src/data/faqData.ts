45b8eaff8f586050595fe3d1ac2c3c270048fb8d
nathantkn <trkhoinguyen.ptcnn@gmail.com>
2025-11-01 19:48:44 -0500

    initial component setup and dropdown arrows

---
+++ src/data/faqData.ts
@@ -0,0 +1,47 @@
+export interface FAQItem {
+  question: string;
+  answer: string;
+}
+
+export const faqData: FAQItem[] = [
+  {
+    question: "What is a hackathon?",
+    answer: "Funsies!",
+  },
+  {
+    question: "Who can participate?",
+    answer: "All UIC students! No expertise needed.",
+  },
+  {
+    question: "When do applications close?",
+    answer: "to be added",
+  },
+  {
+    question: "Is attending SparkHacks free?",
+    answer: "Yes!",
+  },
+  {
+    question: "How big are the teams? Can I apply as a team/solo?",
+    answer: "to be added",
+  },
+  {
+    question: "Does SparkHacks provide food, drinks, or accommodations?",
+    answer: "to be added",
+  },
+  {
+    question: "Can I attend SparkHacks remotely?",
+    answer: "to be added",
+  },
+  {
+    question: "What should I bring?",
+    answer: "to be added",
+  },
+  {
+    question: "Do I have to select a track for my project?",
+    answer: "to be added",
+  },
+  {
+    question: "Any other questions?",
+    answer: "to be added.",
+  },
+];
