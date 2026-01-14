e73dcc46c2301dd75f45ee54a9574f03152cc6a8
Josephine <jbl.noetic@gmail.com>
2026-01-13 21:29:32 -0600

    hiding schedule

---
+++ src/data/scheduleData.ts
@@ -0,0 +1,111 @@
+// no spoilers! subject to change
+export const scheduleData = [
+    {
+        date: "February 6th",
+        day: "Friday",
+        events: [
+            {
+                "title": "Check-In",
+                "time": "5:00 PM - 6:00  PM",
+                "description": "Have your QR code ready and get your teammates together for check-in.",
+                "tags": ["❗ Mandatory","Snacks", "Location: TBD"]
+            },
+            {
+                "title": "Networking",
+                "time": "5:00 PM - 6:30 PM",
+                "description": "This is your chance to network with our AWESOME sponsors in a casual environment",
+                "tags": ["Career", "Internship", "Location: TBD"]
+            },
+            {
+                "title": "Opening Ceremony",
+                "time": "6:00 PM - 6:30 PM",
+                "description": "Join us for the big night! We'll introduce our sponsors, reveal the tracks and prizes.",
+                "tags": ["❗ Mandatory", "Location: TBD"]
+            },
+            {
+                "title": "Workshop: Figma",
+                "time": "7:00 PM - 8:00 PM",
+                "description": "Learn the basics of Figma and bring your ideas to life visually!",
+                "tags": ["Workshop", "Location: TBD"]
+            },
+            {
+                "title": "Sponsor Tabling",
+                "time": "7:00 PM - 8:00 PM",
+                "description": "Meet and interact with our sponsors at their tables.",
+                "tags": ["Social"]
+            },
+            {
+                "title": "Dinner 🍿",
+                "time": "8:15 PM - 9:30 PM",
+                "description": "Enjoy some top tier food! Vegetarian options available.",
+                "tags": ["Food", "Location: TBD"]
+            },
+            {
+                "title": "De-Stress: Mario Kart!",
+                "time": "10:00 PM - 11:00 PM",
+                "description": "Take a breather, compete with friends in Mario Kart, and enjoy some midnight snacks. Insomia cookies provided!",
+                "tags": ["Social", "Snacks", "Location: TBD"]
+            },
+        ],
+    },
+    {
+        date: "February 7th",
+        day: "Saturday",
+        events: [
+            {
+                "title": "Check-In & Breakfast",
+                "time": "9:00am - 10:00am",
+                "description": "Check in for Day 2 and re-fuel with breakfast! Vegetarian options available.",
+                "tags": ["❗ Mandatory", "Food", "Location: TBD"]
+            },
+            {
+                "title": "Lunch 🍿",
+                "time": "1:00 PM - 2:00 PM",
+                "description": "It's the final stretch! Make sure to grab food with your team before your final submission.",
+                "tags": ["Food", "Location: TBD"]
+            },
+            {
+                "title": "‼️ DevPost Drafts Due",
+                "time": "2:30 PM - 2:45 PM",
+                "description": "Make sure drafts for DevPosts have been made and teams have been submitted!",
+                "tags": ["❗ Mandatory"]
+            },
+            {
+                "title": "De-Stress: Origami",
+                "time": "4:00 PM - 6:30 PM",
+                "description": "Unwind and enjoy a hands-on origami session.",
+                "tags": ["Social", "Location: TBD"]
+            },
+            {
+                "title": "‼️ Hacking Over",
+                "time": "5:30 PM - 5:45 PM",
+                "description": "YOU DID IT! FINAL SUBMISSIONS! NO LATE POSTS ALLOWED.",
+                "tags": ["❗ Mandatory"]
+            },
+            {
+                "title": "Demo/Presentation Workshop",
+                "time": "6:30 PM - 7:00 PM",
+                "description": "Learn tips and strategies on delivering an engaging project presentation.",
+                "tags": ["Social", "Career", "Location: TBD"]
+            },
+            {
+                "title": "Project Expo",
+                "time": "7:00 PM - 8:00 PM",
+                "description": "Showcase your projects to our sponsors while judging takes place.",
+                "tags": ["Social", "Career"]
+            },
+            {
+                "title": "Dinner & Meet the Team! 🍿",
+                "time": "8:00 PM - 9:00 PM",
+                "description": "Enjoy your well-earned dinner and meet the SparkHacks team!",
+                "tags": ["Food", "Location: TBD"]
+            },
+            {
+                "title": "Closing Ceremony & Awards",
+                "time": "9:00 PM - 9:30 PM",
+                "description": "Winner announcements and closing remarks!",
+                "tags": ["❗ Mandatory", "Location: TBD"]
+            },
+        ]
+    }
+]
