e10b5c0310973f6a4de2be99fd9b41345d33feae
Josephine <jbl.noetic@gmail.com>
2026-01-19 22:31:58 -0600

    schedule update

--- src/data/scheduleData.ts
+++ src/data/scheduleData.ts
@@ -5,16 +5,16 @@ export const scheduleData = [
         day: "Friday",
         events: [
             {
+                "title": "Sponsor Tabling",
+                "time": "5:00 PM - 7:30 PM",
+                "description": "This is your chance to network with our AWESOME sponsors in a casual environment.",
+                "tags": ["Social", "Career", "Location: TBD"]
+            },
+            {
                 "title": "Check-In",
                 "time": "5:00 PM - 6:00  PM",
                 "description": "Have your QR code ready and get your teammates together for check-in.",
                 "tags": ["❗ Mandatory","Snacks", "Location: TBD"]
-            },
-            {
-                "title": "Networking",
-                "time": "5:00 PM - 6:30 PM",
-                "description": "This is your chance to network with our AWESOME sponsors in a casual environment",
-                "tags": ["Career", "Internship", "Location: TBD"]
             },
             {
                 "title": "Opening Ceremony",
@@ -23,27 +23,27 @@ export const scheduleData = [
                 "tags": ["❗ Mandatory", "Location: TBD"]
             },
             {
-                "title": "Workshop: Figma",
-                "time": "7:00 PM - 8:00 PM",
-                "description": "Learn the basics of Figma and bring your ideas to life visually!",
+                "title": "Workshop: Aedify",
+                "time": "6:30 PM - 7:30 PM",
+                "description": "Join our wonderful sponsor Aedify for their workshop!",
                 "tags": ["Workshop", "Location: TBD"]
             },
             {
-                "title": "Sponsor Tabling",
-                "time": "7:00 PM - 8:00 PM",
-                "description": "Meet and interact with our sponsors at their tables.",
-                "tags": ["Social"]
+                "title": "Workshop: Argonne",
+                "time": "7:30 PM - 8:30 PM",
+                "description": "Join our wonderful sponsor Argonne for their workshop!",
+                "tags": ["Workshop", "Location: TBD"]
             },
             {
                 "title": "Dinner 🍿",
-                "time": "8:15 PM - 9:30 PM",
+                "time": "8:15 PM - 9:00 PM",
                 "description": "Enjoy some top tier food! Vegetarian options available.",
                 "tags": ["Food", "Location: TBD"]
             },
             {
                 "title": "De-Stress: Mario Kart!",
-                "time": "10:00 PM - 11:00 PM",
-                "description": "Take a breather, compete with friends in Mario Kart, and enjoy some midnight snacks. Insomia cookies provided!",
+                "time": "8:30 PM - 9:30 PM",
+                "description": "Take a breather, compete with friends in Mario Kart, and enjoy some midnight snacks!",
                 "tags": ["Social", "Snacks", "Location: TBD"]
             },
         ],
@@ -71,22 +71,16 @@ export const scheduleData = [
                 "tags": ["❗ Mandatory"]
             },
             {
-                "title": "De-Stress: Origami",
+                "title": "De-Stress: Charades!",
                 "time": "4:00 PM - 6:30 PM",
-                "description": "Unwind and enjoy a hands-on origami session.",
+                "description": "Unwind and enjoy a game of charades with your fellow hackers!",
                 "tags": ["Social", "Location: TBD"]
             },
             {
                 "title": "‼️ Hacking Over",
-                "time": "5:30 PM - 5:45 PM",
+                "time": "6:00 PM",
                 "description": "YOU DID IT! FINAL SUBMISSIONS! NO LATE POSTS ALLOWED.",
                 "tags": ["❗ Mandatory"]
-            },
-            {
-                "title": "Demo/Presentation Workshop",
-                "time": "6:30 PM - 7:00 PM",
-                "description": "Learn tips and strategies on delivering an engaging project presentation.",
-                "tags": ["Social", "Career", "Location: TBD"]
             },
             {
                 "title": "Project Expo",
